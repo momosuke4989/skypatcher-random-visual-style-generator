141260ef104bc62a5f9eff286672dc19dc245d8d
momosuke4989 <momosuke4989@gmail.com>
2026-04-11 19:07:09 +0900

    Exclude child NPC

--- SP_RandomVisualStyle Genarator - FormListGenerator.pas
+++ SP_RandomVisualStyle Genarator - FormListGenerator.pas
@@ -12,7 +12,7 @@ var
 procedure AssignNPCToFormList(e, formListRecord: IwbMainRecord);
 var
   formIDList, newFormList: IInterface;
-  
+
 begin
   formIDList := ElementByName(formListRecord, 'FormIDs');
   newFormList := ElementAssign(formIDList, HighInteger, nil, False);
@@ -46,7 +46,7 @@ begin
   slBasicRaces := TStringList.Create;
   slExcludeRaces := TStringList.Create;
   lNewFormListRecords := TList.Create;
-  
+
   slBasicRaces.Add('Nord');
   slBasicRaces.Add('Imperial');
   slBasicRaces.Add('Breton');
@@ -57,7 +57,7 @@ begin
   slBasicRaces.Add('Orc');
   slBasicRaces.Add('Khajiit');
   slBasicRaces.Add('Argonian');
-  
+
   slExcludeRaces.Add('DefaultRace');
   slExcludeRaces.Add('InvisibleRace');
   slExcludeRaces.Add('ManekinRace');
@@ -65,7 +65,7 @@ begin
   slExcludeRaces.Add('DLC1NordRace');
   slExcludeRaces.Add('NordRaceAstrid');  // 正常に機能すると思うが見た目がホラーなので
   slExcludeRaces.Add('DoremoraRace');  // 多分追加しても問題ないがとりあえず除外指定
-  
+
   // ユーザーにファイル名を入力してもらう
   if not InputQuery('New Plugin name entry', 'Enter the Form List Plugin name (e.g. MyPlugin.esp)', pluginName) then
   begin
@@ -81,10 +81,10 @@ begin
     Result := 1;
     Exit;
   end;
-  
+
   // プレフィックスにアンダースコアを追加
   formListPrefix := formListPrefix + '_';
-  
+
   // ファイル名の拡張子を確認し、必要に応じて追加
   if LowerCase(ExtractFileExt(pluginName)) <> '.esp' then
     pluginName := pluginName + '.esp';
@@ -100,9 +100,9 @@ begin
 
   AddMessage('A new plugin has been created:' + pluginName);
   AddMasterIfMissing(newPlugin, 'Skyrim.esm');
-  
+
   AddMessage('Form List Prefix:' + formListPrefix);
-  
+
   // Editor ID のリストを初期化
   // 全員および性別
   slNewFormListEditorIDs.Add(formListPrefix + 'All' + EDITORIDSUFFIX);
@@ -114,7 +114,7 @@ begin
     slNewFormListEditorIDs.Add(formListPrefix + slBasicRaces[i] + 'Male' + EDITORIDSUFFIX);
     slNewFormListEditorIDs.Add(formListPrefix + slBasicRaces[i] + 'Female' + EDITORIDSUFFIX);
   end;
-  
+
   // レコードを直接追加できないので、バニラのFormListレコードをコピー
   baseFormList := RecordByFormID(FileByIndex(0), DRAUGRWEAPONS, True);
   // 各 Editor ID に対して FormList を作成
@@ -126,7 +126,7 @@ begin
       AddMessage('Failed to create FormList:' + slNewFormListEditorIDs[i]);
       Continue;
     end;
-    
+
     // 'FormIDs' サブレコードを取得、エントリがあれば削除
     entries := ElementByPath(newFormList, 'FormIDs');
     if Assigned(entries) then begin
@@ -155,85 +155,92 @@ var
   raceFaceGenHeadFlag: boolean;
 begin
   Result := 0;
-  
+
   // NPCレコード以外のレコードはスキップ
   if Signature(e) <> 'NPC_' then begin
     AddMessage('This record is not an NPC record.');
     Exit;
   end;
-  
+
   AddMessage('Record to operate on:' + EditorID(e));
-  
+
   // レコードが所属するプラグインをマスター指定する
   AddMasterIfMissing(newPlugin, GetFileName(GetFile(e)));
-  
+
   // NPCレコードフラグを取得
   NPCFlag := GetElementNativeValues(e, 'ACBS\Flags');
   templateFlag := GetElementNativeValues(ElementBySignature(e, 'ACBS'), 'Template Flags');
-  
+
   // UseTraitsテンプレートフラグを持つNPCはスキップ
   if (templateFlag and $01) <> 0 then begin
     AddMessage('This record uses a template and has the Use Traits flag.');
     AddMessage('Skip the processing record.');
     Exit;
   end;
-  
+
   // Editor IDにTemplateを含むNPCはスキップ
   if Pos('template', LowerCase(EditorID(e))) > 0 then begin
     AddMessage('This record is a NPC template.');
     AddMessage('Skip the processing record.');
     Exit;
   end;
-  
+
+  // Editor IDにchildを含むNPCはスキップ
+  if Pos('child', LowerCase(EditorID(e))) > 0 then begin
+    AddMessage('This record is a child NPC.');
+    AddMessage('Skip the processing record.');
+    Exit;
+  end;
+
   // playerレコードはスキップ
   if Pos('player', LowerCase(EditorID(e))) > 0 then begin
     AddMessage('This record is player.');
     AddMessage('Skip the processing record.');
     Exit;
   end;
-  
+
   // CharGen Presetはスキップ
   if (NPCFlag and $4) = 4 then begin
     AddMessage('This record is a CharGen Prest.');
     AddMessage('Skip the processing record.');
     Exit;
   end;
-  
-  
+
+
   // 種族の取得
   raceRecord := LinksTo(ElementByPath(e, 'RNAM'));
   raceString := EditorID(raceRecord);
-  
+
   // FaceGenHeadフラグを持たない種族はスキップ
   if not GetRaceFaceGenHeadFlag(raceRecord) then begin
     AddMessage(raceString + ' doesn''t have FaceGen Head Flag. Skip processing.');
     Exit;
   end;
-  
+
   //AddMessage(raceString + ' has FaceGen Head Flag. Continue processing.');
-  
+
   // 種族名からRace部分を取り除く
   Delete(raceString, Length(raceString) - 3, 4);
   //AddMessage('raceString:' + raceString);
-  
+
   // 性別の判定
   if (NPCFlag and $1) = 0 then
     NPCGender := 'Male'
   else
     NPCGender := 'Female';
-  
+
   // 除外種族は処理をスキップ
   if slExcludeRaces.IndexOf(raceString) <> -1 then begin
     AddMessage('Skip exclude races');
     Exit;
   end;
-  
+
   // 基本種族以外は処理をスキップ
   if slBasicRaces.IndexOf(raceString) = -1 then begin
     AddMessage('Skip non-basic races');
     Exit;
   end;
-  
+
   // EditorIDを基にFormListを取得（ lNewFormListRecords を先に対応づけ済み）
   // 共通: "All" は全員に割り当て
   indxAll := slNewFormListEditorIDs.IndexOf(formListPrefix + 'All' + EDITORIDSUFFIX);
@@ -252,7 +259,7 @@ end;
 function Finalize: integer;
 begin
   Result := 0;
-  
+
   if Assigned(slNewFormListEditorIDs) then
     slNewFormListEditorIDs.Free;
   if Assigned(slBasicRaces) then
