23bf469336d3ec6aa7fd7ff171b168e8e5b8777e
momosuke4989 <momosuke4989@gmail.com>
2026-04-25 20:43:31 +0900

    improve-output

--- SP_RandomVisualStyle Genarator - ConfigGenerator.pas
+++ SP_RandomVisualStyle Genarator - ConfigGenerator.pas
@@ -7,29 +7,40 @@ const
 
 var
   // イニシャル処理で設定・使用する変数
+  slBasicRaces: TStringList;
   slRVSFactionName: TStringList;
   RVSGFileName: string;
   addRVS: boolean;
-  
+
   // プロセス処理で設定する変数
   formListPrefix: string;
 
 procedure AssignRVSExportString(const slRVSFactionName, prefix: string; var slExportString: TStringList);
 var
   RVSOperation: string;
+  i: Cardinal;
 begin
-  
+
   if addRVS = true then
     RVSOperation := 'add'
   else
     RVSOperation := 'set';
-    
-    slExportString.Add(';' + slRVSFactionName);
-    slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':filterByEditorIdContainsExcluded=Template:restrictToGender=male:' + RVSOperation + 'RandomVisualStyle=' + prefix + '_Male_RVSG~' + APPLYCHANCE);
 
-    slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':filterByEditorIdContainsExcluded=Template:restrictToGender=female:' + RVSOperation + 'RandomVisualStyle=' + prefix + '_Female_RVSG~' + APPLYCHANCE);
-    
-  
+    slExportString.Add(';' + slRVSFactionName);
+    slExportString.Add(';Restrict to Gender Only');
+    slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':rvsRestrictToTraits=true:restrictToGender=male:' + RVSOperation + 'RandomVisualStyle=' + prefix + '_Male_RVSG~' + APPLYCHANCE);
+
+    slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':rvsRestrictToTraits=true:restrictToGender=female:' + RVSOperation + 'RandomVisualStyle=' + prefix + '_Female_RVSG~' + APPLYCHANCE);
+
+    slExportString.Add(#13#10);
+    slExportString.Add(';Restrict to Race and Gender');
+    for i := 0 to slBasicRaces.Count - 1 do
+    begin
+      slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':rvsRestrictToTraits=true:restrictToGender=male:restrictToRace=' + slBasicRaces[i] + ':' + RVSOperation + 'RandomVisualStyle=' + prefix + '_' + slBasicRaces[i] + '_Male_RVSG~' + APPLYCHANCE);
+      slExportString.Add('filterByEditorIdContains=Enc, ' + slRVSFactionName + ':rvsRestrictToTraits=true:restrictToGender    =female:restrictToRace=' + slBasicRaces[i] + ':' + RVSOperation + 'RandomVisualStyle=' + prefix + '_' + slBasicRaces  [i] + '_Female_RVSG~' + APPLYCHANCE);
+    end;
+
+
 end;
 
 function Initialize: integer;
@@ -39,11 +50,23 @@ var
   i: Integer;
 begin
   Result := 0;
-  slRVSFactionName   := TStringList.Create;
-  
+  slBasicRaces     := TStringList.Create;
+  slRVSFactionName := TStringList.Create;
+
   RVSGFileName     := '';
-  
+
   disableOpts      := TStringList.Create;
+
+  slBasicRaces.Add('NordRace');
+  slBasicRaces.Add('ImperialRace');
+  slBasicRaces.Add('BretonRace');
+  slBasicRaces.Add('RedguardRace');
+  slBasicRaces.Add('HighElfRace');
+  slBasicRaces.Add('WoodElfRace');
+  slBasicRaces.Add('DarkElfRace');
+  slBasicRaces.Add('OrcRace');
+  slBasicRaces.Add('KhajiitRace');
+  slBasicRaces.Add('ArgonianRace');
 
   slRVSFactionName.Add('Bandit=false');
   slRVSFactionName.Add('Warlock=false');
@@ -63,17 +86,17 @@ begin
   slRVSFactionName.Add('Penitus=false');
   slRVSFactionName.Add('Afflicted=false');
   slRVSFactionName.Add('Cultist=false');
-  
+
   checkBoxCaption := 'Target faction select';
-  
+
   if MessageDlg(
     'Select which Random Visual Style Operation:' + #13#10 +
-    'Yes = Add' + #13#10 +
+    'Yes = Add (Recommend)' + #13#10 +
     'No = Set',
     mtConfirmation, [mbYes, mbNo], 0
     ) = mrYes then
     addRVS := true;
-  
+
   // 各オプションの設定
   try
     if ShowCheckboxForm(slRVSFactionName, disableOpts, checkBoxCaption) then
@@ -88,11 +111,11 @@ begin
       Result := -1;
       Exit;
     end;
-    
+
   finally
     disableOpts.Free;
   end;
-  
+
 end;
 
 function Process(e: IInterface): integer;
@@ -102,64 +125,64 @@ var
 begin
   Result := 0;
   underscorePos := 0;
-  
+
   formListEditorID := '';
-  
+
   // FormListレコードでなければスキップ
   if Signature(e) <> 'FLST' then begin
     AddMessage(GetElementEditValues(e, 'EDID') + ' is not Form List record.');
     Exit;
   end;
-  
+
   if RVSGFileName = '' then begin
     RVSGFileName := GetFileName(GetFile(e));
     RVSGFileName := ChangeFileExt(RVSGFileName, '');
   end;
-  
+
   if formListPrefix = '' then begin
     formListEditorID := GetElementEditValues(e, 'EDID');
     underscorePos := Pos('_', formListEditorID);
     formListPrefix := Copy(formListEditorID, 1, underscorePos - 1);
   end;
-  
+
   //AddMessage(RVSGFileName);
   //AddMessage(formListPrefix);
-  
+
 end;
 
 function Finalize: integer;
 var
   // 設定ファイル出力用変数
   slExport: TStringList;
-  
+
   dlgSave: TSaveDialog;
   exportFileName, saveDir, filterString, fileExtension: string;
   RVSOperation: string;
   i: Cardinal;
 begin
   Result := 0;
-  
+
   if RVSGFileName = '' then begin
     AddMessage('The information required to generate a configuration file could not be obtained.');
     Exit;
   end;
-  
+
   slExport         := TStringList.Create;
-  
+
   if addRVS = true then
     RVSOperation := 'ADD - '
   else
     RVSOperation := 'SET - ';
-  
+
   // 出力設定
   saveDir := DataPath + 'SkyPatcher Random Visual Style Generator\SKSE\Plugins\SkyPatcher\npc\SkyPatcher Random Visual Style Generator\';
   filterString := 'Ini (*.ini)|*.ini';
   fileExtension := '.ini';
-    
+
   // ディレクトリ作成
   if not DirectoryExists(saveDir) then
     ForceDirectories(saveDir);
-    
+
   dlgSave := TSaveDialog.Create(nil);
   try
     // ファイル保存
@@ -185,7 +208,7 @@ begin
     slExport.Free;
   if Assigned(slRVSFactionName) then
     slRVSFactionName.Free;
-    
+
 end;
 
 end.
